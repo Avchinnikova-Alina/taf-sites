2cbeac4a52234ce44ae7c253560b7eb7ac3e8de3
Avchinnikova Alina <avchinnikova1012@gmail.com>
2023-05-23 00:56:12 +0300

    add pizzatempo page and test

---
+++ src/main/java/by/itacademy/avchinnikova/alina/taf/sites/PizzatempoPage.java
@@ -0,0 +1,7 @@
+package by.itacademy.avchinnikova.alina.taf.sites;
+
+public class PizzatempoPage {
+    String inputEmailXpath = "/html/body/div/div[1]/form/p[1]/input";
+    String inputPasswordXpath = "/html/body/div/div[1]/form/p[2]/input[1]";
+    String buttonSignInXpath = "/html/body/div/div[1]/form/p[2]/input[2]";
+}
---
+++ src/test/java/by/itacademy/avchinnikova/alina/taf/sites/PizzatempoTest.java
@@ -0,0 +1,64 @@
+package by.itacademy.avchinnikova.alina.taf.sites;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PizzatempoTest {
+    ChromeDriver driver = new ChromeDriver();
+    PizzatempoPage page = new PizzatempoPage();
+
+    @BeforeEach
+    public void warmUp() {
+        driver.get("https://www.pizzatempo.by/");
+    }
+
+    @Test
+    public void emptyEmailAndEmptyPassword() {
+        WebElement submitSignInButton = driver.findElement(By.xpath(page.buttonSignInXpath));
+        submitSignInButton.click();
+    }
+
+    @Test
+    public void incorrectEmail() {
+        WebElement inputEmail = driver.findElement(By.xpath(page.inputEmailXpath));
+        inputEmail.sendKeys("avchinnikova1012");
+        WebElement buttonSignIn = driver.findElement(By.xpath(page.buttonSignInXpath));
+        buttonSignIn.click();
+    }
+
+    @Test
+    public void testPizzatempoWithEmptyEmailAndSomePassword() {
+        WebElement inputPassword = driver.findElement(By.xpath(page.inputPasswordXpath));
+        inputPassword.sendKeys("qwerty1");
+        WebElement buttonSignIn = driver.findElement(By.xpath(page.buttonSignInXpath));
+        buttonSignIn.click();
+    }
+
+    @Test
+    public void correctEmailAndEmptyPassword() {
+        WebElement inputEmail = driver.findElement(By.xpath(page.inputEmailXpath));
+        inputEmail.sendKeys("avchinnikova1012@gmail.com");
+        WebElement buttonSignIn = driver.findElement(By.xpath(page.buttonSignInXpath));
+        buttonSignIn.click();
+    }
+
+    @Test
+    public void correctEmailAndPassword() {
+        WebElement inputEmail = driver.findElement(By.xpath(page.inputEmailXpath));
+        inputEmail.sendKeys("avchinnikova1012@gmail.com");
+        WebElement inputPassword = driver.findElement(By.xpath(page.inputPasswordXpath));
+        inputPassword.sendKeys("qwerty1");
+        WebElement buttonSignIn = driver.findElement(By.xpath(page.buttonSignInXpath));
+        buttonSignIn.click();
+    }
+
+    @AfterEach
+    public void tearsDown() {
+
+        driver.quit();
+    }
+}
