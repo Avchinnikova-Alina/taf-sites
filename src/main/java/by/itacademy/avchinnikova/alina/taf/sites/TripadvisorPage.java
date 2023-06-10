04d064a49a3943a5d284a430df67fc11607055ca
Avchinnikova Alina <77278516+Avchinnikova-Alina@users.noreply.github.com>
2023-06-10 21:06:38 +0300

    Delete TripadvisorPage.java

--- src/main/java/by/itacademy/avchinnikova/alina/taf/sites/TripadvisorPage.java
+++
@@ -1,9 +0,0 @@
-package by.itacademy.avchinnikova.alina.taf.sites;
-
-public class TripadvisorPage {
-    String buttonRegisterXpath = "//*[@id='lithium-root']/header/div/nav/div/div[2]/a[3]";
-    String buttonContinueXpath ="//*[@id='ssoButtons']/button";
-    String inputEmailXpath ="//*[@id='regSignIn.email']";
-    String inputPasswordXpath ="//*[@id='regSignIn.password']";
-    String buttonSignInXpath = "//*[@id='regSignIn']/div[4]/button[1]";
-}
